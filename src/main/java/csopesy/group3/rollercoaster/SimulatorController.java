db4c83ff21f11731e4b06dccd4c785798ff510b8
raphroncal <raph.roncal01@gmail.com>
2022-06-14 05:17:06 +0800

    Refactored code

--- src/main/java/csopesy/group3/rollercoaster/SimulatorController.java
+++ src/main/java/csopesy/group3/rollercoaster/SimulatorController.java
@@ -40,73 +40,25 @@ public class SimulatorController {
         int C = model.nCapacity;
 
         // initialize circles
-        Circle[] passenger = new Circle[n];                         // circles will represent the passengers
-        AnimationTimer[] animationWander = new AnimationTimer[n];   // each passenger has their own timer
+        Circle[] passenger = new Circle[n];     // circles will represent the passengers
         for(int i = 0; i < n; i++) {
             passenger[i] = new Circle(0, 0, size, Color.WHITE);
             panePassenger.getChildren().add(passenger[i]);
             setPosition(passenger[i]);
-
-            int finalI = i;
-            animationWander[i] = new AnimationTimer() {
-                private long lastUpdate;
-                @Override
-                public void handle(long l) {
-                    // 1_000_000_000 (1M milliseconds) = 1 second
-                    if (l - lastUpdate >= 1_000_000_000) {
-                        wander(passenger[finalI]);
-                        lastUpdate = l;
-                    }
-
-                    // TODO: if condition for passenger to stop wandering and start waiting to board the ride
-//                    if(some condition) {
-//                        stop();
-//                        goToQueue(passenger[finalI]);
-//                    }
-                }
-            };
+            // all passengers are initially wandering
+            wander(passenger[i]);
         }
 
-        boolean test = true;
         // initialize rectangles
-        Rectangle[] car = new Rectangle[m];                         // rectangles will represent the cars
-        AnimationTimer[] animationArrive = new AnimationTimer[m];   // each car has their own timer
+        Rectangle[] car = new Rectangle[m];     // rectangles will represent the cars
         for(int i = 0; i < m; i++) {
             car[i] = new Rectangle(100, 200, Color.YELLOW);
             car[i].setTranslateX(220);
             car[i].setTranslateY(675);
             paneCar.getChildren().add(car[i]);
-
-            int finalI = i;
-            animationArrive[i] = new AnimationTimer() {
-                private long lastUpdate;
-                @Override
-                public void handle(long l) {
-                    if (l - lastUpdate >= 10_000_000) {
-                        drive(car[finalI]);
-                        lastUpdate = l;
-                    }
-
-                    // TODO: car stops and can start boarding passengers
-                    if(car[finalI].getTranslateY() == 400.0) {
-                        stop();
-
-                        // TODO: model methods
-
-                        // after boarding passengers, car can now start driving again
-//                        start();
-                    }
-
-                }
-            };
         }
 
-        // call this to make passenger[0] start wandering
-        animationWander[0].start();
-        // call this to make passenger[0] stop wandering
-//        animationWander[0].stop();
-
-        animationArrive[0].start();
+        drive(car[0]);
     }
 
     /**
@@ -135,40 +87,63 @@ public class SimulatorController {
         int distance = 25;  // how far the circles will "walk"
         double paneWidth = panePassenger.getWidth() - (size * 2);
         double paneHeight = panePassenger.getHeight() - (size * 2);
-        double x = c.getTranslateX();
-        double y = c.getTranslateY();
 
-        int action = ThreadLocalRandom.current().nextInt(0, 5 + 1);
-        switch (action) {
-            // cases 0 & 5 means circle will not move
-            // move right
-            case 1 -> {
-                c.setTranslateX(x + distance);
-                if(c.getTranslateX() > paneWidth)
-                    c.setTranslateX(x - distance);
-            }
+        AnimationTimer animation = new AnimationTimer() {
+            private long lastUpdate;
+            @Override
+            public void handle(long l) {
+                // 1_000_000_000 (1M milliseconds) = 1 second
+                if (l - lastUpdate >= 1_000_000_000) {
+                    double x = c.getTranslateX();
+                    double y = c.getTranslateY();
 
-            // move left
-            case 2 -> {
-                c.setTranslateX(x - distance);
-                if(c.getTranslateX() < 0)
-                    c.setTranslateX(x + distance);
-            }
+                    int action = ThreadLocalRandom.current().nextInt(0, 5 + 1);
+                    switch (action) {
+                        // cases 0 & 5 means circle will not move
+                        // move right
+                        case 1 -> {
+                            c.setTranslateX(x + distance);
+                            if(c.getTranslateX() > paneWidth)
+                                c.setTranslateX(x - distance);
+                        }
 
-            // move up
-            case 3 -> {
-                c.setTranslateY(y - distance);
-                if(c.getTranslateY() < 0)
-                    c.setTranslateY(y + distance);
-            }
+                        // move left
+                        case 2 -> {
+                            c.setTranslateX(x - distance);
+                            if(c.getTranslateX() < size)
+                                c.setTranslateX(x + distance);
+                        }
 
-            // move down
-            case 4 -> {
-                c.setTranslateY(y + distance);
-                if(c.getTranslateY() > paneHeight)
-                    c.setTranslateY(y - distance);
+                        // move up
+                        case 3 -> {
+                            c.setTranslateY(y - distance);
+                            if(c.getTranslateY() < size)
+                                c.setTranslateY(y + distance);
+                        }
+
+                        // move down
+                        case 4 -> {
+                            c.setTranslateY(y + distance);
+                            if(c.getTranslateY() > paneHeight)
+                                c.setTranslateY(y - distance);
+                        }
+                    }
+
+                    lastUpdate = l;
+                }
+
+                // TODO: if condition for passenger to stop wandering and start waiting to board the ride
+//                    if(some condition) {
+//                        stop();
+//                        goToQueue(c);
+//                    }
             }
-        }
+        };
+
+        // call this to make this specific passenger start wandering
+        animation.start();
+        // call this to make this specific passenger start wandering
+//        animation.stop();
     }
 
     /**
@@ -177,8 +152,36 @@ public class SimulatorController {
      * @param r is a rectangle representing a car;
      */
     private void drive(Rectangle r) {
-        double y = r.getTranslateY();
-        r.setTranslateY(y - 1);
+        AnimationTimer animation = new AnimationTimer() {
+            private long lastUpdate;
+            @Override
+            public void handle(long l) {
+                if (l - lastUpdate >= 10_000_000) {
+                    double y = r.getTranslateY();
+                    System.out.println(y);
+                    r.setTranslateY(y - 1);
+
+                    lastUpdate = l;
+                }
+
+                // TODO: car stops and can start boarding passengers
+                if(r.getTranslateY() == 400.0) {
+                    stop();
+
+                    // TODO: model methods to board passengers
+                    // after boarding passengers, car can now start driving again
+                    start();
+                }
+
+                // reset car position when it goes out of bounds
+                if(r.getTranslateY() == -200.0) {
+                    stop();
+                    resetCarPosition(r);
+                }
+            }
+        };
+
+        animation.start();
     }
 
     /**
